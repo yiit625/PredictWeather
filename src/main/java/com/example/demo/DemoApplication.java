31b7d27261d2a6ec178c5f467e7ae2997dd3f95b
yigitsahin <yigit.sahin@denk-it.com>
2021-04-10 18:11:10 +0300

    Rapid Api Implementation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.serviceimpl.ServiceImpl;
+import com.example.demo.services.Services;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.squareup.okhttp.*;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DemoApplication {
 
+
+	public DemoApplication() throws IOException {
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		ServiceImpl serviceImpl = new ServiceImpl();
+
+		Response response = serviceImpl.checkWeather();
+
+		System.out.println(response);
 	}
 
 }
---
+++ src/main/java/com/example/demo/serviceimpl/ServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.demo.serviceimpl;
+
+import com.example.demo.services.Services;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class ServiceImpl implements Services {
+
+    @Override
+    public Response checkWeather() {
+     // https://rapidapi.com/sangatpuria01/api/weather-com/endpoints
+    Response response = null;
+    try {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://weather-com.p.rapidapi.com/v3/wx/disease/tracker/countryList/current?language=en")
+                .get()
+                .addHeader("x-rapidapi-key", "f15361ddf9msh28bb1e5eddebea5p16c2d3jsn54743eea0a7f")
+                .addHeader("x-rapidapi-host", "weather-com.p.rapidapi.com")
+                .build();
+
+        response = client.newCall(request).execute();
+
+    }
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+        return response;
+    }
+
+    
+
+}
