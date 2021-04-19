b79d983655c8fdeb9281323901c74893ec5227c2
ilaydac <ilaydacolaak@hotmail.com>
2021-04-19 13:19:15 +0300

    add hadoop jars

---
+++ src/main/java/org/hadoop/training/WordCount.java
@@ -0,0 +1,23 @@
+package org.hadoop.training;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.Tool;
+
+public class WordCount implements Tool{
+
+	public Configuration getConf() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setConf(Configuration arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int run(String[] arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
